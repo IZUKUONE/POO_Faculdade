c06c11a1b991b19997fe714d56eecd12db74f587
JorgeL <carvalhojorge501@gmail.com>
2025-07-30 12:08:18 -0300

    Associação pt 02

--- src/POO/Association/player.java
+++ src/POO/Association/player.java
@@ -3,13 +3,26 @@ package POO.Association;
 public class player {
 
     private String name;
+    private time Time;
 
     public void imprime(){
         System.out.println(name);
+        //sem ele vai da um NULL POINTER, pois o jogador precisa de um time
+        if (Time != null){
+            System.out.println(Time.getName());
+        }
     }
 
     public String getName() {
         return name;
+    }
+
+    public time getTime() {
+        return Time;
+    }
+
+    public void setTime(time time) {
+        Time = time;
     }
 
     public void setName(String name) {
---
+++ src/POO/Association/time.java
@@ -0,0 +1,17 @@
+package POO.Association;
+
+public class time {
+    private String name;
+
+    public time(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
