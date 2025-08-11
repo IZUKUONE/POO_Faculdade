16365e531e16522037edcee8cdbdc287c2f22ae1
JorgeL <carvalhojorge501@gmail.com>
2025-08-11 07:56:25 -0300

    Execicio

---
+++ src/nelioAlves/sequentialStructure/exercise.java
@@ -0,0 +1,19 @@
+package nelioAlves.sequentialStructure;
+
+public class exercise {
+    public static void main(String[] args) {
+        String product1 = "Apple";
+        String product2 = "Computer";
+
+        int age = 30;
+        int code = 5000;
+        char gender = 'F';
+
+        double price1 = 500.0;
+        double price2 = 600.0;
+        double measure = 53.235658;
+
+        System.out.printf("Product\n %s, witch price is %.2f \n %s, witch price is %.2f \n Record: %d years old, code %d and gender: %c",
+                product1, price1, product2, price2, age, code, gender);
+    }
+}
