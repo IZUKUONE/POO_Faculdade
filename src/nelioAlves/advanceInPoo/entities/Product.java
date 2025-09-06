bb8c83d61ff2ce81f55e273458135d08474304f5
JorgeL <carvalhojorge501@gmail.com>
2025-09-06 18:52:47 -0300

    construtores

--- src/nelioAlves/advanceInPoo/entities/Product.java
+++ src/nelioAlves/advanceInPoo/entities/Product.java
@@ -1,11 +1,16 @@
 package nelioAlves.advanceInPoo.entities;
 
 public class Product {
-    public String name;
+    public String name; // atributo do metodo
     public double price;
     public int quantity;
     public double totalValueInStock() {
         return price * quantity;
+    }              //parameto
+    public Product(String name, double price, int quantity) {
+        this.name = name; // this = referencia o atributo do metodo, não o parametro
+        this.price = price;
+        this.quantity = quantity;
     }
     public void addProducts(int quantity) {
         this.quantity += quantity;
