d4d98193e909a6ac0fe4f6c205b92ff0e539b7d0
JorgeL <carvalhojorge501@gmail.com>
2025-09-12 10:55:03 -0300

    saque

--- src/nelioAlves/advanceInPoo/exercise/build/Program.java
+++ src/nelioAlves/advanceInPoo/exercise/build/Program.java
@@ -41,7 +41,12 @@ public class Program {
         System.out.println("Updated account data: ");
         System.out.println(account);
 
-
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        double withdrawValue = sc.nextDouble();
+        account.withdraw(withdrawValue);
+        System.out.println("Updated account data: ");
+        System.out.println(account);
         sc.close();
     }
 }
