155c1ddfc51ca0bb7d9af2a46347c81c995be7ea
JorgeL <carvalhojorge501@gmail.com>
2025-09-30 19:07:27 -0300

    questao01

---
+++ src/POO/jocelio/OO/revisao/questao01/entitie/Automovel.java
@@ -0,0 +1,28 @@
+package POO.jocelio.OO.revisao.questao01.entitie;
+
+public class Automovel {
+    public String chassi;
+    public String modelo;
+    public String marca;
+    public double capacidadeDoTanque;
+    public double quantidadeAtual;
+    public double consumo;
+    public double distancia;
+
+    public Automovel() {
+    }
+
+    public void andar(double distancia) {
+        if (consumo <= 0) {
+            System.out.println("Erro: consumo inválido (zero ou negativo).");
+            return;
+        }
+
+        double litrosGastos = distancia / consumo;
+        quantidadeAtual -= litrosGastos;
+
+        if (quantidadeAtual < 0) {
+            quantidadeAtual = 0;
+        }
+    }
+}
