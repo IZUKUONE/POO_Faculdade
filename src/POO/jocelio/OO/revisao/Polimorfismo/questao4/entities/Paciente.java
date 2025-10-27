56d346a8c55db807c2ac0a767060f0c206e8b280
JorgeL <carvalhojorge501@gmail.com>
2025-10-27 17:17:10 -0300

    classe produto

---
+++ src/POO/jocelio/OO/revisao/Polimorfismo/questao4/entities/Paciente.java
@@ -0,0 +1,49 @@
+package POO.jocelio.OO.revisao.Polimorfismo.questao4.entities;
+
+public class Paciente {
+    private String codigo;
+    private String nome;
+    private float debito;
+
+    public Paciente(String codigo, String nome, float debito) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.debito = debito;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getDebito() {
+        return debito;
+    }
+
+    public void usarServico(float valorServico){
+        this.debito += valorServico;
+    }
+
+    public void pagarServico(float valorServico){
+        if(this.debito <= valorServico){
+            this.debito -= valorServico;
+        }else {
+            System.out.println("Valor incoerente");
+        }
+    }
+
+    public void mostrarDados(){
+        System.out.println("Nome: " + this.nome + "\ncodigo: " + this.codigo + "\ndebito: " + this.debito);
+    }
+}
