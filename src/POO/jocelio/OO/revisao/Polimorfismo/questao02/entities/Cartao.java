f1d1d2620469220b9618b9634192efc0741d797f
JorgeL <carvalhojorge501@gmail.com>
2025-10-26 21:33:02 -0300

    cartao

---
+++ src/POO/jocelio/OO/revisao/Polimorfismo/questao02/entities/Cartao.java
@@ -0,0 +1,69 @@
+package POO.jocelio.OO.revisao.Polimorfismo.questao02.entities;
+
+public class Cartao {
+    private String numeroDoCartao;
+    private String titular;
+    private double limiteDeCredito;
+    private double saldoDevedor;
+
+    public Cartao(String numeroDoCartao, String titular, double limiteDeCredito) {
+        this.numeroDoCartao = numeroDoCartao;
+        this.titular = titular;
+        this.limiteDeCredito = 0;
+    }
+
+    public String getNumeroDoCartao() {
+        return numeroDoCartao;
+    }
+    public void setNumeroDoCartao(String numeroDoCartao) {
+        this.numeroDoCartao = numeroDoCartao;
+    }
+    public String getTitular() {
+        return titular;
+    }
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public double getLimiteDeCredito() {
+        return limiteDeCredito;
+    }
+    public void setLimiteDeCredito(double limiteDeCredito) {
+        this.limiteDeCredito = limiteDeCredito;
+    }
+    public double getSaldoDevedor() {
+        return saldoDevedor;
+    }
+
+    public boolean autorizar(double valorCompra){
+        if (valorCompra + saldoDevedor <= limiteDeCredito) {
+            saldoDevedor += valorCompra;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void comprar(double valorCompra){
+        if (autorizar(valorCompra)) {
+            saldoDevedor += valorCompra; // Soma ao saldo devedor
+            System.out.println("Compra autorizada com sucesso!");
+        } else {
+            System.out.println("Erro: Compra não autorizada - limite insuficiente!");
+        }
+    }
+
+    public void pagar(double valorPagamento){
+        if (saldoDevedor != 0){
+            saldoDevedor -= valorPagamento;
+        }else {
+            System.out.println("Você não deve nada");
+        }
+    }
+
+    public void mostrarDados(){
+        System.out.println("Numero do cartao: " + numeroDoCartao
+                + "\nTitular: " + titular
+                + "\nLimite: " + limiteDeCredito
+                + "\nSaldo: " + saldoDevedor);
+    }
+}
---
+++ src/POO/jocelio/OO/revisao/Polimorfismo/questao02/entities/CartaoEspecial.java
@@ -0,0 +1,41 @@
+package POO.jocelio.OO.revisao.Polimorfismo.questao02.entities;
+
+public class CartaoEspecial extends Cartao{
+    private double valorMinimo;
+
+    public CartaoEspecial(String numeroDoCartao, String titular, double limiteDeCredito, double v) {
+        super(numeroDoCartao, titular, limiteDeCredito);
+        this.valorMinimo = limiteDeCredito;
+    }
+    public double getValorMinimo() {
+        return valorMinimo;
+    }
+    public void setValorMinimo(double valorMinimo) {
+        this.valorMinimo = valorMinimo;
+    }
+
+    public void comprar(double valorCompra){
+        if (valorCompra >= valorMinimo) {
+            super.comprar(valorCompra);
+        } else {
+            System.out.println("ERRO: Valor mínimo para compra é R$ " +
+                    String.format("%.2f", valorMinimo));
+        }
+    }
+
+    public void pagarTudo() {
+        double valorPago = getSaldoDevedor();
+        if (valorPago > 0) {
+            pagar(valorPago);
+            System.out.println("Saldo devedor quitado totalmente!");
+        } else {
+            System.out.println("Não há saldo devedor a ser pago.");
+        }
+    }
+
+    public void mostrarDados(){
+        super.mostrarDados();
+        System.out.println("\nValor minimo: " + valorMinimo);
+    }
+
+}
