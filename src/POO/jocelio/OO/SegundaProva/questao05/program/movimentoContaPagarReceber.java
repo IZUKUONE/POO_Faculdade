0f2956fb69ea9edf0c9cb9ce051aad7d164f4b18
JorgeL <carvalhojorge501@gmail.com>
2025-10-28 21:16:34 -0300

    main feito

--- src/POO/jocelio/OO/SegundaProva/questao05/program/movimentoContaPagarReceber.java
+++ src/POO/jocelio/OO/SegundaProva/questao05/program/movimentoContaPagarReceber.java
@@ -8,6 +8,20 @@ public class movimentoContaPagarReceber {
         ContaReceber conta1 = new ContaReceber(1, "mercado Total", 500);
         ContaPagar conta2 = new ContaPagar(2, "Distribuidor ideal", 400);
 
+        conta1.vender(600);
+        conta2.pagar(400);
+        System.out.println("\n=== Após Venda e Compra ===");
+        System.out.println("Conta a Receber: " + conta1.getCodigo() + " - " + conta1.getNome() + " - Saldo: " + conta1.getSaldo());
+        System.out.println("Conta a Pagar: " + conta2.getCodigo() + " - " + conta2.getNome() + " - Saldo: " + conta2.getSaldo());
 
-    }
+        conta1.vender(100);
+        conta2.pagar(300);
+        System.out.println("\n=== Após Receber e Pagar ===");
+        System.out.println("Conta a Receber: " + conta1.getCodigo() + " - " + conta1.getNome() + " - Saldo: " + conta1.getSaldo());
+        System.out.println("Conta a Pagar: " + conta2.getCodigo() + " - " + conta2.getNome() + " - Saldo: " + conta2.getSaldo());
+
+        conta1.vender(1500);
+        conta2.pagar(600);
+        System.out.println("Conta a Receber: " + conta1.getCodigo() + " - " + conta1.getNome() + " - Saldo: " + conta1.getSaldo());
+        System.out.println("Conta a Pagar: " + conta2.getCodigo() + " - " + conta2.getNome() + " - Saldo: " + conta2.getSaldo());}
 }
